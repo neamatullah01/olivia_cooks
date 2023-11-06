40fdac409e8483cfec07636db07584eb2c04bf49
neamatullah01 <neamat22205101908@diu.edu.bd>
2023-11-06 21:35:19 +0600

    React router and tailwind aaded and start the project

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,11 @@
+import NavBar from "../Components/NavBar/NavBar";
+
+const Main = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+        </div>
+    );
+};
+
+export default Main;
