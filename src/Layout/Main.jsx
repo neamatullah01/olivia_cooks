7ca07ee533d6afa3121edce2200346a2a096a270
neamatullah01 <neamat22205101908@diu.edu.bd>
2023-11-09 02:09:27 +0600

    Footer added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../Components/NavBar/NavBar";
+import Footer from "../Components/Footer/Footer";
 
 const Main = () => {
     return (
         <div className="w-[1300px] mx-auto">
             <NavBar></NavBar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
